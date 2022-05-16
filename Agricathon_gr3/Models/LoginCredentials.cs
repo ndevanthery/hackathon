13241d1fe8b558410678b66f50f2ea7740f877c8
ndevanthery <93075753+ndevanthery@users.noreply.github.com>
2022-05-16 11:27:23 +0200

    Add project files.

---
+++ Agricathon_gr3/Models/LoginCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agricathon_gr3.Models
+{
+    public class LoginCredentials
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ Agricathon_gr3/Models/PersonVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agricathon_gr3.Models
+{
+    public class PersonVM
+    {
+        public string username { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string password { get; set; }
+
+    }
+}
