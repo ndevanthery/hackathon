a6068e2d98ef48ded3feb1f228ac0d93d295a8a6
Cristiana <limacoelhocristiana@gmail.com>
2022-05-24 09:22:31 +0200

    Detail Project

---
+++ Agricathon_gr3/Models/ProjectDetailVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agricathon_gr3.Models
+{
+    public class ProjectDetailVM
+    {
+        public int ProjectId { get; set; }
+        public string NameProject { get; set; }
+        public string Place { get; set; }
+        public string Phase { get; set; }
+    }
+}
